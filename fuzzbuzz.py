0c4d52d297ca7dfb1f340356888d26223b55a40e
Madhavi Jakkula <118192831+madhavijakkula@users.noreply.github.com>
2022-12-03 21:45:56 +0530

    Add files via upload
    this repository of fizzbuzz program was made by me

---
+++ fuzzbuzz.py
@@ -0,0 +1,1 @@
+i=1while i <= 100:    if i%3==0:        print("Fizz", end="")        if i%5==0:            print("Buzz", end="")    elif i%5==0:        print("Buzz", end="")    else:        print(i, end="")    print()    i+=1       
